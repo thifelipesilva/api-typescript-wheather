f95847c05bdab540a3ad22671dd6741df88cfc7d
thifelipesilva <thiagophsilva@hotmail.com>
2023-01-31 17:23:27 -0300

    finalizando o forecast service

--- Readme.md
+++ Readme.md
@@ -1,1 +1,8 @@
-token storm glass 'c37f53c4-96bf-11ed-bc36-0242ac130002-c37f5450-96bf-11ed-bc36-0242ac130002'
+Api para previsoes de tempo para uma praia especifica.
+
+em construção...
+
+
+Typescript
+Jest
+TDD
--- src/service/forecast.ts
+++ src/service/forecast.ts
@@ -1,4 +1,5 @@
 import { ForecastPoint, StormGlass } from '@src/clients/stormGlass';
+import { InternalError } from '@src/util/errors/internal-error';
 
 export enum BeachPosition {
   S = 'S',
@@ -15,32 +16,73 @@ export interface Beach {
   user: string;
 }
 
+export interface TimeForecast {
+  time: string;
+  forecast: BeachForecast[];
+}
+
 export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {}
+
+export class ForecastProcessingInternalError extends InternalError {
+  constructor(message: string){
+    super(`Unexpected error during the forecast processing: ${message}`)
+  }
+}
 
 export class Forecast {
   constructor(protected stormGlass = new StormGlass()) {}
 
   public async processForecastForBeaches(
     beaches: Beach[]
-  ): Promise<BeachForecast[]> {
+  ): Promise<TimeForecast[]> {
     const pointsWithCorrectSources: BeachForecast[] = [];
 
-    for (const beach of beaches) {
-      const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
-      const enrichedBeachData = points.map((e) => ({
-        ...{
-          lat: beach.lat,
-          lng: beach.lng,
-          name: beach.name,
-          position: beach.position,
-          rating: 1,
-        },
-        ...e,
-      }));
+    try {
+      for (const beach of beaches) {
+        const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
+        const enrichedBeachData = this.enrichedBeachData(points, beach)
+  
+        pointsWithCorrectSources.push(...enrichedBeachData);
+      }
+  
+      return this.mapForecastByTime(pointsWithCorrectSources); //serializando nossa repsota de um BeachForecast para TimeForecast
+      
+    } catch (error) {
+      throw new ForecastProcessingInternalError((error as Error).message);
+    }
+  }
 
-      pointsWithCorrectSources.push(...enrichedBeachData);
+  private enrichedBeachData(points: ForecastPoint[], beach: Beach): BeachForecast[] {
+    return points.map((e) => ({
+      ...{},
+      ...{
+        lat: beach.lat,
+        lng: beach.lng,
+        name: beach.name,
+        position: beach.position,
+        rating: 1,
+      },
+      ...e,
+    }));
+  }
+
+
+  //serializando a respota para o formato certo
+  private mapForecastByTime(forecast: BeachForecast[]): TimeForecast[] {
+    const forecastByTime: TimeForecast[] = [];
+    for (const point of forecast) {
+      //se ja existir um time, acrescesta o forescast a esse time, se nao, cria um time e add o forecast
+      const timePoint = forecastByTime.find(f => f.time == point.time);
+      if (timePoint) {
+        timePoint.forecast.push(point);
+      } else {
+        forecastByTime.push({
+          time: point.time,
+          forecast: [point],
+        })
+      }
     }
 
-    return pointsWithCorrectSources;
+    return forecastByTime;
   }
 }
