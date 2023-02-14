740c760e2803192d0530f882c5ed19301d0e4031
thifelipesilva <thiagophsilva@hotmail.com>
2023-02-13 23:33:27 -0300

    Adicionando o beach model

--- src/database.ts
+++ src/database.ts
@@ -1,7 +1,8 @@
 import config, { IConfig } from 'config';
-import mongoose, { connect as mongooseConnect, connection } from 'mongoose';
+import { connect as mongooseConnect, connection } from 'mongoose';
 
 const dbConfig: IConfig = config.get('App.database');
+
 export const connect = async (): Promise<void> => {
     await mongooseConnect(dbConfig.get('mongoUrl'));
 };
