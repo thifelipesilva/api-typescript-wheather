437e1080f9c8973f2085f40a0a0f5e314903a9f2
thifelipesilva <thiagophsilva@hotmail.com>
2023-04-29 22:57:51 -0300

    criando rota me para obter info do usuario

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,9 +1,10 @@
-import { Controller, Post } from '@overnightjs/core';
+import { Controller, Get, Middleware, Post } from '@overnightjs/core';
 import { User } from '@src/models/user';
 import AuthService from '@src/service/auth';
 import { Request, Response } from 'express';
 import { BaseController } from '.';
 import logger from '@src/logger';
+import { authMiddleware } from '@src/middlewares/auth';
 
 @Controller('users')
 export class UsersController extends BaseController {
@@ -42,5 +43,22 @@ export class UsersController extends BaseController {
     }
     const token = AuthService.generateToken(user.toJSON());
     return res.status(200).send({ token: token });
+  }
+
+  @Get('me')
+  @Middleware(authMiddleware)
+  public async me(
+    req: Request,
+    res: Response
+  ): Promise<Response> {
+    const email = req.decoded ? req.decoded.email : undefined;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return this.sendErrorResponse(res, {
+        code: 404,
+        message: 'User not found!',
+      });
+    }
+    return res.send({ user });
   }
 }
